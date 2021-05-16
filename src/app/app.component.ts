9b73215b00a5c8f196f020ea7db6782641858969
jpenariasholberton <1875@holbertonschool.com>
2021-05-16 18:09:55 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'consultaSaldo';
+  // cuenta: {nombre: string, cuentaNumero: string, saldo: number} = {
+  //   "nombre": "Cuenta de Ahorros",
+  //   "cuentaNumero": "xxxxxxxxx85",
+  //   "saldo": 100000
+  // }
+  cuentas: {nombre: string, cuentaNumero: string, saldo: number}[] = [
+    {
+      "nombre": "Cuenta de Ahorros",
+      "cuentaNumero": "xxxxxxxxx85",
+      "saldo": 100000
+    },
+    {
+      "nombre": "CDT",
+      "cuentaNumero": "xxxxxxxxx20",
+      "saldo": 100000
+    },
+    {
+      "nombre": "Cuenta Corriente",
+      "cuentaNumero": "xxxxxxx7789",
+      "saldo": 100000
+    }
+  ]
+  usuario: string = "Jiseth"
 }
